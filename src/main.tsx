72f38cd681debb75eb90f53392112854a3f4be6f
Guilherme Müller <54514054+Guilhermemuller13@users.noreply.github.com>
2025-09-08 23:01:43 -0300

    :construction: add dat.gui

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import "./index.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import Stats from "three/examples/jsm/libs/stats.module.js";
+import { GUI } from "dat.gui";
 
 const scene = new THREE.Scene();
 
@@ -34,6 +35,16 @@ scene.add(cube);
 const stats = new Stats();
 
 document.body.appendChild(stats.dom);
+
+const gui = new GUI();
+const cubeFolder = gui.addFolder("Cube");
+
+cubeFolder.add(cube.rotation, "x", 0, Math.PI * 2);
+cubeFolder.add(cube.rotation, "y", 0, Math.PI * 2);
+cubeFolder.add(cube.rotation, "z", 0, Math.PI * 2);
+
+const cameraFolder = gui.addFolder("Camera");
+cameraFolder.add(camera.position, "z", 0, 20);
 
 function animate() {
   requestAnimationFrame(animate);
